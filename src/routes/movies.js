09b426711b860cadea770f7eb12941e1cb355ace
AnsgarGM <loxn52@gmail.com>
2021-12-21 15:17:52 -0600

    API con base de datos en MySQL

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,54 +1,81 @@
 const { Router } = require( 'express' );
 const router = Router();
 const _ = require( 'underscore' );
+const mysql = require( 'mysql' );
 
-const movies = require( '../example.json' );
-//console.log( movies ); 
+// MySQL
+const connection = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'api-node'
+} );
 
+connection.connect( error => {
+    if( error ){
+        console.log( error );
+    }else{
+        console.log( 'Database server running' );
+    }
+} );
+
+// Metodos y rutas
 router.get( '/', ( req, res ) => {
-    res.json( movies );
+    //res.json( connection );
+    const sql = 'SELECT * FROM movies';
+    connection.query( sql, ( err, result ) => {
+        if( err ) throw err;
+        if( result.length > 0 ){
+            res.json( result );
+        } else {
+            res.send( 'No hay registros en base de datos' );
+        }
+    } );
 } );
 
 router.post( '/', ( req, res ) => {
-    const { titulo, director, anio, rating } = res.body;
+    const { titulo, director, anio, rating } = req.body;
+    //console.log( req.body );
+    const sql = 'INSERT INTO movies SET ?';
     if( titulo && director && anio && rating ){
-        const id = movies.length + 1;
-
-        const newmovie = { id, ...req.body };
-        movies.push( newmovie );
-        res.json( movies );
+        const newobj = {
+            titulo: titulo,
+            director: director,
+            anio: anio,
+            rating, rating
+        }
+        connection.query( sql, newobj, error => {
+            if( error ){
+                res.send( error );
+            }else{
+                res.send( 'Agregado' );
+            }
+            
+        } );
+        
+        //res.json( movies );
     }else{
         res.send( 'Bad request' );
     }
-    res.send( 'Recibido' );
 } );
 
 router.delete( '/:id', ( req, res ) => {
     const { id } = req.params;
-    _.each( movies, ( movie, i ) => {
-        if( movie.id == id ){
-            movies.splice( i, 1 );
-        }
+    const sql = `DELETE FROM movies WHERE id = '${ id }'`;
+    connection.query( sql, error => {
+        if( error ) throw error;
+        res.send( 'Eliminado' );
     } );
-    res.json( movies );
 } );
 
 router.put( '/:id', ( req, res ) => {
     const { id } = req.params;
-    const { titulo, director, anio, rating } = res.body;
-    if( titulo && director && anio && rating ){
-        _.each( movies, ( movie, i ) =>{
-            if( movie.id == id ){
-                movie.titulo = titulo;
-                movie.director = director;
-                movie.anio = anio;
-                movie.rating = rating;
-            }
-        } );
-        res.json( movies );
-    }else{
-        res.json( { error: "Bad id" } );
-    }
+    const { titulo, director, anio, rating } = req.body;
+    const sql = `UPDATE movies SET titulo = '${ titulo }', director = '${ director }', anio = '${ anio }', rating = '${ rating }' WHERE id = ${ id }`;
+    connection.query( sql, error => {
+        if( error ) throw error;
+        res.send( 'Película actualizada' );
+    } );
 } );
 
 module.exports = router
