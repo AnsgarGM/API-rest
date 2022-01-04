05af7279e30db17b9a48d3b53285b7563393f143
AnsgarGM <loxn52@gmail.com>
2022-01-03 19:06:30 -0600

    Configuración de CORS

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ app.use( morgan( 'dev' ) );
 app.use( express.urlencoded( { extended: false } ) );
 app.use( express.json() );
 app.use( archivo.any() );
+app.use((req, res, next) => {   // CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Rutas
 app.use( require( './routes/index' ) );
